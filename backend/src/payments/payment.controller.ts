282dcb5401cc222385a2aa5d4fbf9d25836e7b52
EmmanuelMose <emmanuelmose806@gmail.com>
2025-10-29 12:13:15 +0300

    10 commit

---
+++ backend/src/payments/payment.controller.ts
@@ -0,0 +1,165 @@
+import { Request, Response } from "express";
+import * as paymentService from "../payments/payment.service";
+
+// get all payments
+export const getAllPaymentsController = async (_req: Request, res: Response) => {
+  try {
+    const payments = await paymentService.getAllPayments()
+    res.status(200).json({ data: payments })
+    return;
+  } catch (error: any) {
+    console.log("Error fetching payments:", error)
+    res.status(500).json({ error: error.message || "Internal Server Error" })
+    return;
+  }
+};
+
+//get payment by ID controller
+export const getPaymentByIdController = async (req: Request, res: Response) => {
+  try {
+    const id = parseInt(req.params.id);
+
+    if (isNaN(id)) {
+       res.status(400).json({ message: "Invalid ID" });
+      return;
+    }
+
+    const payment = await paymentService.getPaymentById(id);
+
+    if (!payment) {
+      res.status(404).json({ message: "Payment not found" });
+      return; 
+    }
+
+    res.status(200).json({ data: payment });
+
+    return;
+  } catch (error: any) {
+     res.status(500).json({ error: error.message })
+    return;
+  }
+};
+
+//Create a payment controller
+export const createPaymentController = async (req: Request, res: Response) => {
+  try {
+
+    const body = req.body;
+
+    if (body.paymentDate) body.paymentDate = new Date(body.paymentDate);
+
+    const payment = await paymentService.createPayment(body);
+
+    if (!payment) {
+      res.status(400).json({ message: "Payment not created" });
+      return;
+    }
+    res.status(201).json({ message: "Payment created successfully", payment });
+    return;
+  } catch (error: any) {
+    console.log("Error creating payment:", error);
+     res.status(500).json({ error: error.message || "Internal Server Error" });
+    return;
+  }
+};
+
+//Update payment controller
+export const updatePaymentController = async (req: Request, res: Response) => {
+  try {
+    const id = parseInt(req.params.id);
+
+    if (isNaN(id)) {
+      res.status(400).json({ message: "Invalid ID" });
+      return;
+    }
+
+    const body = req.body;
+
+    if (body.paymentDate) body.paymentDate = new Date(body.paymentDate);
+    if (body.createdAt) body.createdAt = new Date(body.createdAt);
+    if (body.updatedAt) body.updatedAt = new Date(body.updatedAt);
+
+    const updated = await paymentService.updatePayment(id, body);
+    if (!updated) {
+      return res.status(404).json({ message: "Payment not found" });
+    }
+    res.status(200).json({ message: "Payment updated successfully", payment: updated });
+    return;
+  } catch (error: any) {
+    console.log("Error updating payment:", error);
+    res.status(500).json({ error: error.message || "Internal Server Error" });
+    return;
+  }
+};
+
+//Delete payment controller
+export const deletePaymentController = async (req: Request, res: Response) => {
+  try {
+    const id = parseInt(req.params.id, 10);
+
+    if (isNaN(id)) {
+      res.status(400).json({ message: "Invalid ID" });
+      return;
+    }
+
+    await paymentService.deletePayment(id);
+    res.status(204).send();
+    return;
+  } catch (error: any) {
+    console.log("Error deleting payment:", error);
+    res.status(500).json({ error: error.message || "Internal Server Error" });
+    return;
+  }
+};
+
+// Get payments by userID
+export const getPaymentsByUserIdController = async (req: Request, res: Response) => {
+  try {
+    const userID = Number(req.params.userID);
+
+    if (isNaN(userID)) {
+      res.status(400).json({ error: "Invalid user ID" });
+      return;
+    }
+
+    const payments = await paymentService.getPaymentByUserId(userID);
+
+    if (!payments || payments.length === 0) {
+      res.status(404).json({ message: "No payments found for this user." });
+      return;
+    }
+
+    res.status(200).json({ data: payments });
+    return;
+  } catch (error: any) {
+    console.log("Error fetching payments by user ID:", error);
+    res.status(500).json({ error: error.message || "Internal Server Error" });
+    return;
+  }
+};
+
+// Get payments by bookingID
+export const getPaymentsByBookingIdController = async (req: Request, res: Response) => {
+  try {
+    const bookingID = Number(req.params.bookingID);
+
+    if (isNaN(bookingID)) {
+      res.status(400).json({ error: "Invalid booking ID" });
+      return;
+    }
+
+    const payments = await paymentService.getPaymentByBookingId(bookingID);
+
+    if (!payments || payments.length === 0) {
+      res.status(404).json({ message: "No payments found for this booking." });
+      return;
+    }
+
+    res.status(200).json({ data: payments });
+    return;
+  } catch (error: any) {
+    console.log("Error fetching payments by booking ID:", error);
+    res.status(500).json({ error: error.message || "Internal Server Error" });
+    return;
+  }
+};
---
+++ backend/src/payments/payment.router.ts
@@ -0,0 +1,90 @@
+import { Express } from "express";
+import { createPaymentController, deletePaymentController, getAllPaymentsController, 
+    getPaymentByIdController, getPaymentsByBookingIdController, getPaymentsByUserIdController, updatePaymentController } from "./payment.controller";
+
+
+
+
+const payment = (app: Express) => {
+    // create payment
+    app.route("/api/payment").post(
+        async (req, res, next) => {
+            try {
+                await createPaymentController(req, res)
+            } catch (error) {
+                next(error)
+            }
+        }
+    )
+
+    //get all payments
+    app.route("/api/payments").get(
+        async (req, res, next) => {
+            try {
+                await getAllPaymentsController(req, res)
+            } catch (error) {
+                next(error)
+            }
+        }
+    )
+
+    //get payment by ID
+    app.route("/api/payment/:id").get(
+        async (req, res, next) => {
+            try {
+                await getPaymentByIdController(req, res)
+            } catch (error) {
+                next(error)
+            }
+        }
+    )
+
+    //update payment
+    app.route("/api/payment/:id").put(
+        async (req, res, next) => {
+            try {
+                await updatePaymentController(req, res)
+            } catch (error) {
+                next(error)
+            }
+        }
+    )
+
+    //delete payment
+    app.route("/api/payment/:id").delete(
+        async (req, res, next) => {
+            try {
+                await deletePaymentController(req, res)
+            } catch (error) {
+                next(error)
+            }
+        }
+    )
+
+    // Get payments by user ID
+     app.route("/api/payments/user/:userID").get(
+         async (req, res, next) => {
+             try {
+                 await getPaymentsByUserIdController(req, res);
+            } catch (error) {
+                 next(error);
+             }
+            }
+         );
+
+    // Get payments by booking ID
+     app.route("/api/payments/booking/:bookingID").get(
+         async (req, res, next) => {
+             try {
+                 await getPaymentsByBookingIdController(req, res);
+            } catch (error) {
+                 next(error);
+             }
+            }
+         );
+}
+
+
+export default payment;
+
+
