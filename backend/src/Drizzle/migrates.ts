ba6118e7db6a8d21f30dd22ed3d7e7732574f31a
EmmanuelMose <emmanuelmose806@gmail.com>
2025-10-30 12:02:34 +0300

    17 commit

--- backend/src/Drizzle/migrates.ts
+++ backend/src/Drizzle/migrates.ts
@@ -1,6 +1,6 @@
-import "dotenv/config";
-import { migrate } from "drizzle-orm/node-postgres/migrator";
-import db, { client } from "./db"
+// import "dotenv/config";
+// import { migrate } from "drizzle-orm/node-postgres/migrator";
+// import db, { client } from "./db"
 
 // async function migration() {
 //     console.log("......Migrations Started......");
