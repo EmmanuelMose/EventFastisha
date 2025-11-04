20200c4655dda99c39578096ea3d41007e6f20b3
EmmanuelMose <emmanuelmose806@gmail.com>
2025-11-04 11:26:11 +0300

    49 commit

--- frontend/src/components/nav/Navbar.tsx
+++ frontend/src/components/nav/Navbar.tsx
@@ -2,7 +2,7 @@ import { useDispatch, useSelector } from "react-redux";
 import logo from "../../assets/images/logo.png"
 import { NavLink, useNavigate } from 'react-router';
 import type { RootState } from "../../app/store";
-import { logout } from "../../Features/login/userSlice";
+import { logout } from "../../Features/login/userSlicer";
 
 const Navbar = () => {
     const userrole = useSelector((state: RootState) => state.user.user?.role);
