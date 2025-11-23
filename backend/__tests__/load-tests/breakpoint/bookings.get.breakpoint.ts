e9f0aaa129bfb3e87819bc69e3deebc26c035cf8
EmmanuelMose <emmanuelmose806@gmail.com>
2025-11-23 08:24:17 +0300

    breakpoint 2 commit

--- backend/__tests__/load-tests/breakpoint/bookings.get.breakpoint.ts
+++ backend/__tests__/load-tests/breakpoint/bookings.get.breakpoint.ts
@@ -1,7 +1,7 @@
 import http from 'k6/http';
 import { check, sleep } from 'k6';
 
-const BASE_URL = 'http://localhost:8081'; 
+const BASE_URL = 'https://eventfastisha.onrender.com'; 
 
 export const options = {
     stages: [
